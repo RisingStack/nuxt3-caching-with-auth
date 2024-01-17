04d45e31a959e6eb7e41af9b996bdc3599709b19
TroyanOlga <lurryhurry@gmail.com>
2024-01-17 11:41:22 +0100

    add console.logs to debug

--- server/api/auth.ts
+++ server/api/auth.ts
@@ -1,7 +1,7 @@
 import { users } from "~/dbFake";
 
 export default defineEventHandler((event) => {
-    const loggedIn = users[0].loggedIn;
-    return { loggedIn };
-  });
-  
+  const loggedIn = users[0].loggedIn;
+  console.log("API", users[0], loggedIn);
+  return { loggedIn };
+});
